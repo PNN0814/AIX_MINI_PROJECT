47e5868dd91fb74ae4e25f0cf000621e53003bf9
폰냥냥 <wjdrlghd09@naver.com>
2025-09-04 12:15:06 +0900

    Update common.py
    공통 코드 추가

--- AIX_MINI_PROJECT/WebContent/webpage/common/common.py
+++ AIX_MINI_PROJECT/WebContent/webpage/common/common.py
@@ -15,3 +15,12 @@ IMG_RESULT_CAP_SK_DIR   = os.path.join(IMG_RESULT_DIR, "capture_skeleton")
 IMG_RESULT_MAT_DIR      = os.path.join(IMG_RESULT_DIR, "matching")
 IMG_RESULT_MAT_SK_DIR   = os.path.join(IMG_RESULT_DIR, "matching_skeleton")
 IMG_RESULT_VIDEO_DIR    = os.path.join(IMG_RESULT_DIR, "video")
+
+# music(백그라운드 음악)의 경로
+MUSIC_DIR = os.path.join(BASE_DIR, "music")
+
+#pages(html, css, js)의 경로
+PAGES_DIR       = os.path.join(BASE_DIR, "webpage", "pages")
+PAGES_HTML_DIR  = os.path.join(PAGES_DIR, "html")
+PAGES_CSS_DIR   = os.path.join(PAGES_DIR, "css")
+PAGES_JS_DIR    = os.path.join(PAGES_DIR, "js")
